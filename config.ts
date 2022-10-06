e4178af87f431988863543fbadfefc8f2e4dfd4d
Tikeswar Naik <Tikeswar.naik11@gmail.com>
2022-10-06 19:53:04 +0530

    fix posts per page count

--- config.ts
+++ config.ts
@@ -1,3 +1,6 @@
 export const SITE_TITLE = "Coders blog";
 export const SITE_DESCRIPTION =
   "A blog website for front-end web developers. Informative Blogs in an easy language with interacive example are posted here.";
+export const POSTS_PER_PAGE = 6;
+export const SNIPPETS_PER_PAGE = 9;
+export const POPULAR_POSTS_COUNT = 6;
